1325595c238eb52cabbde50724c2f715f3ecadf3
MrEshbobotev <mreshboboyev@gmail.com>
2025-03-03 21:39:47 +0500

    Write Domain codes for Customer aggregate root: Events, factories, value objects

---
+++ src/Core/Shop.Domain/Entities/CustomerAggregate/EGender.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel;
+using System.Text.Json.Serialization;
+
+namespace Shop.Domain.Entities.CustomerAggregate;
+
+[JsonConverter(typeof(JsonStringEnumConverter<EGender>))]
+public enum EGender
+{
+    None = 0,
+
+    Male = 1,
+
+    Female = 2
+}
