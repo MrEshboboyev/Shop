91c5e85af0e5e6681707b9bb2df308e9f97a48e9
MrEshbobotev <mreshboboyev@gmail.com>
2025-03-04 13:22:16 +0500

    Create PublicApi api project (with error handling middleware and api responses); Update solution : .NET 9 -> .NET 10

---
+++ src/Presentation/Shop.PublicApi/Models/ApiResponseT.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Shop.PublicApi.Models;
+
+public sealed class ApiResponse<T> : ApiResponse
+{
+    [JsonConstructor]
+    public ApiResponse(T result, bool success, string successMessage, int statusCode, IEnumerable<ApiErrorResponse> errors)
+        : base(success, successMessage, statusCode, errors)
+    {
+        Result = result;
+    }
+
+    public ApiResponse()
+    {
+    }
+
+    public T Result { get; private init; }
+
+    public static ApiResponse<T> Ok(T result) =>
+        new() { Success = true, StatusCode = StatusCodes.Status200OK, Result = result };
+
+    public static ApiResponse<T> Ok(T result, string successMessage) =>
+        new() { Success = true, StatusCode = StatusCodes.Status200OK, Result = result, SuccessMessage = successMessage };
+
+    public static ApiResponse<T> Created(T result) =>
+        new() { Success = true, StatusCode = StatusCodes.Status201Created, Result = result };
+}
---
+++ src/Presentation/Shop.PublicApi/Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
